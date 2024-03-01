4e80b7424416592939842b25da60439e873f7a8e
sabaprogrammer <nikachikvaidze321@gmail.com>
2024-03-01 14:26:50 +0400

    commit

---
+++ day7/homework/day7homework.py
@@ -0,0 +1,11 @@
+print("different actions Various different results follow, Oppressing the weak is incorrect, i always lossing against experienced chess players, i can handle working 9 hours nonstop, i can tell you everything i know, My friends conducted a survey. ")
+
+age = input("Enter Your age: ")
+age1 = input("Enter Your age: ")
+age2 = input("Enter Your age: ")
+age3 = input("Enter Your age: ")
+age4 = input("Enter Your age: ")
+
+print(int(age) + int(age1))
+print(int(age2) // (int(age) + int(age1)))
+print((int(age3) + int(age4)) * (int(age) + int(age1)))
