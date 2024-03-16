cab530101602e99bf7ec4d62f79304bae2287b0a
sabaprogrammer <nikachikvaidze321@gmail.com>
2024-03-16 22:11:52 +0400

    commit

---
+++ day 11/homework/homework3.py
@@ -0,0 +1,15 @@
+
+
+for i in range(0, 10, 2):
+    print('GOA')
+
+for i in range(0, 10, 2):
+    print(i)
+
+for i in range(0, 10):
+    for j in range(10, 20):
+        for h in range(20, 30):
+            print(i, j, h)
+
+
+
