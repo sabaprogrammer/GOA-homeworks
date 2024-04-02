7389640b6f6cd08823df1ec1e52b40f4ba7d705f
sabaprogrammer <nikachikvaidze321@gmail.com>
2024-04-02 16:18:11 +0400

    commit

---
+++ day 15/classwork/classwork.py
@@ -0,0 +1,13 @@
+for i in "hello everyone":
+    print (i)
+
+for i in "goa is best":
+    print (i)
+
+for i in "i love my mentors":
+    print (i)
+   
+
+for i in range(5):
+    print("hello everyone")
+
