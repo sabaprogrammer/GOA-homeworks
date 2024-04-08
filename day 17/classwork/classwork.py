03e88da7ca7dba7edce26d8d1f334652d02b0222
sabaprogrammer <nikachikvaidze321@gmail.com>
2024-04-08 14:32:50 +0400

    commit

---
+++ day 17/classwork/classwork.py
@@ -0,0 +1,27 @@
+#             0         1      2     3  4    5   6   7      8    9      10
+# products = ["milk", "bread", "oil", 12, 23, 41, 0.1, 0.5, 4.5, True, False]
+# print(products[1])
+
+# name = "saba"
+# surname = "chikvaidze"
+# profession = "student"
+
+# person_info = [name, surname, profession]
+# print(person_info)
+
+# products = ["milk", "bread", "oil", 12, 23, 41, 0.1, 0.5, 4.5, True, False]
+
+# my_products=products[2]
+# print(my_products)
+
+# products = ["milk", "bread", "oil", 12, 23, 41, 0.1, 0.5, 4.5, True, False]
+# products[1] = 'apple'
+# products[0] = 'me var chikva'
+# print(products)
+
+# products = ["milk", "bread", "oil", 12, 23, 41, 0.1, 0.5, 4.5, True, False]
+# print(products[1] + products[2])
+
+nums = [1, 3 , 5, 6, 7, 8]
+for i in nums:
+    print(i)
