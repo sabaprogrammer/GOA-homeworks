0e362a2205bebae63e56397a9fa980467eda3ed7
sabaprogrammer <nikachikvaidze321@gmail.com>
2024-04-14 22:06:21 +0400

    commit

--- day19/classwork/classwork.py
+++ day19/classwork/classwork.py
@@ -1,11 +1,9 @@
-# name = "saba"
-# print(name[2],name[3])
+name = "saba"
+print(name[2],name[3])
 
-# products = ["milk", "bread", "oil", 12, 23, 41, 0.1, 0.5, 4.5, True, False]
+products = ["milk", "bread", "oil", 12, 23, 41, 0.1, 0.5, 4.5, True, False]
 
-# print(products[5:])
-# print(products[7:])
-# name = "saba"
-# print(name[0:3])
-c = ['$', '£', '€', '¥']
-print(c[-3:-1])
+print(products[5:])
+print(products[7:])
+name = "saba"
+print(name[0:3])
