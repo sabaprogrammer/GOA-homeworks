1f2cc3286e4bc232818f1708ccbbf318608e0fe2
sabaprogrammer <nikachikvaidze321@gmail.com>
2024-06-30 00:06:39 +0400

    commit

---
+++ vs test/test.py
@@ -0,0 +1,27 @@
+# saba_age = 15
+# print("sabas age after 5 years will be" + ' ' +str(saba_age+5))
+# age= input ("enter your age: ")
+# print(int(age) + 5)
+# age= input ("enter your age: ")
+# print(int(age) * 7 // 3 + (4*9))
+
+# score1 = int(input("user_score1: "))
+# score2 = int(input("user_score2: "))
+# score3 = int(input("user_score3: "))
+# score4 = int(input("user_score4: "))
+
+# print((score1+score2+score3+score4)//4)
+# for i in range(30):
+#     print("goa")
+# for i in range(5,151):
+#     print(i)
+# for i in range(2,51,4):
+#     print(i)
+# for i in range(1,11):
+#     print(i,"goa")
+# while 1>0:
+#     print("goa")
+word = "example"
+
+for i in range(len(word) - 1, -1, -1):
+    print(word[i], end="")
