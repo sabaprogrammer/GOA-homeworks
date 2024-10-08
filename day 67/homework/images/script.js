e02193b07a9c2a42cca74501d98986f908134d43
sabaprogrammer <157908334+sabaprogrammer@users.noreply.github.com>
2024-10-08 22:26:59 +0400

    commit

---
+++ day 67/homework/images/script.js
@@ -0,0 +1,29 @@
+let a = 10;
+let b = 5;
+
+let sum = a + b;
+console.log(sum);
+
+let difference = a - b;
+console.log(difference);
+
+let product = a * b;
+console.log(product);
+
+let quotient = a / b;
+console.log(quotient);
+
+let power = a ** 2;
+console.log(power);
+
+let remainder = a % b;
+console.log(remainder);
+
+let negative = -a;
+console.log(negative);
+
+a++;
+console.log(a);
+
+b--;
+console.log(b);
